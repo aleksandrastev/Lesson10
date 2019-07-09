2851ab3c9fb1fddd3ad958ba7709ceb3fe4c5691
aleksandrastev <leafsoul91@gmail.com>
2019-07-09 22:11:07 +0200

    Method isPalindrome added to Palindrome class

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -1,12 +1,19 @@
 package exercises;
 
 import java.util.List;
-
 import examples.FileHelper;
 
 public class Palindrome {
 
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
+	}
+
+	public boolean isPalindrome(String word) {
+		for (int i = 0; i < word.length() / 2; i++)
+			if (word.charAt(i) != word.charAt(word.length() - i - 1))
+				return false;
+		return true;
+
 	}
 }
