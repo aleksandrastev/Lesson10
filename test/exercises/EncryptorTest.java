81dd92886675219fa261c21c3f90ade9d6cdc939
aleksandrastev <leafsoul91@gmail.com>
2019-07-09 22:06:10 +0200

    Test for the decrypt method added to EncryptorTest

--- test/exercises/EncryptorTest.java
+++ test/exercises/EncryptorTest.java
@@ -18,6 +18,10 @@ class EncryptorTest {
 	}
 	
 	// Exercise: decrypt this sentence: Nkxg"nqpi"cpf"rtqurgt#
+	@Test
+	void testDecrypt() throws Exception {
+		assertEquals("Live long and prosper!", Encryptor.decrypt("Nkxg\"nqpi\"cpf\"rtqurgt#"));
+	}
 	
 	
 }
