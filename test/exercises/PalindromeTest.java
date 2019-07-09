c26d687830c22c01a806e46cf23c94b44fcf6e9c
aleksandrastev <leafsoul91@gmail.com>
2019-07-09 22:12:22 +0200

    Tests added to PalindromeTest

--- test/exercises/PalindromeTest.java
+++ test/exercises/PalindromeTest.java
@@ -5,16 +5,32 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PalindromeTest {
-	
+
 	Palindrome testPalindrome = new Palindrome();
 
 	@Test
 	void testLoadWords() {
 		assertTrue(testPalindrome.loadWords().contains("apple"));
 	}
-	
-	// 2. Test that a word exists in the dictionary 
-	
+
+	// 2. Test that a word exists in the dictionary
+	@Test
+	void testWordExists() throws Exception {
+		assertTrue(testPalindrome.loadWords().contains("level"));
+		assertTrue(testPalindrome.loadWords().contains("murdrum"));
+		assertTrue(testPalindrome.loadWords().contains("book"));
+
+		assertFalse(testPalindrome.loadWords().contains("agfhjk"));
+
+	}
+
 	// 3. Test that a word is a palindrome
+	@Test
+	void testIsPalindrome() throws Exception {
+		assertTrue(testPalindrome.isPalindrome("level"));
+		assertTrue(testPalindrome.isPalindrome("murdrum"));
+
+		assertFalse(testPalindrome.isPalindrome("book"));
+	}
 
 }
